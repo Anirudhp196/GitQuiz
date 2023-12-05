eebd5b93e878abff103d1ea1fe88b4d653f09c4f
= <anirudhp196@gmail.com>
2023-12-05 14:03:36 -0500

    Created lambda function with functional interface

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
 public class Main {
-    
+    public static void main(String [] args){
+        
+        Add s = (x, y) -> {
+            int result = 0;
+            for(int i = x; i <= y; i++){
+                result += i;
+            }
+
+            return result;
+        }; 
+    }
 }
